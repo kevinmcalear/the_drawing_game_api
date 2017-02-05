02b3bb0d3d0c3072cb475dc840359c26123b9c09
kevinmcalear <kevin.mcalear@gmail.com>
2017-02-04 21:06:59 -0500

    sets up basic parts of game

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170205015242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "game_started"
+    t.integer  "max_players"
+    t.integer  "min_players"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
---
+++ spec/factories/games.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :game do
+    name "MyString"
+    game_started false
+    max_players 1
+    min_players 1
+  end
+end
