968cb3f06141162a1a51f07b7d4ada0e9c56f24b
kevinmcalear <kevin.mcalear@gmail.com>
2017-02-04 22:13:20 -0500

    add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :games
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'api' do
+    namespace :v1 do
+      resources :games
+    end
+  end
 end
